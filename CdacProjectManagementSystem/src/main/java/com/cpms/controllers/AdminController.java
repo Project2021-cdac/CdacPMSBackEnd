20869a4377fac8272572304059a8850d31818430
rithika59 <rithikaraokottala@gmail.com>
2021-01-20 00:40:34 +0530

    Added cross origin * in controllers

--- CdacProjectManagementSystem/src/main/java/com/cpms/controllers/AdminController.java
+++ CdacProjectManagementSystem/src/main/java/com/cpms/controllers/AdminController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -36,6 +37,7 @@ import com.cpms.services.ITechnologyService;
 import com.cpms.services.IUserAccountService;
 import com.cpms.services.StudentService;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping(value = "/admin")
 public class AdminController {
