d8cbe8895767e8ce386f4c2930325bc84853c808
Joji Joy <jojijoy1593@gmail.com>
2021-02-01 21:18:30 +0530

    IProjectService nterface method parameter changed

--- CdacProjectManagementSystem/src/main/java/com/cpms/services/IProjectService.java
+++ CdacProjectManagementSystem/src/main/java/com/cpms/services/IProjectService.java
@@ -2,12 +2,12 @@ package com.cpms.services;
 
 import java.util.List;
 
+import com.cpms.pojos.Course;
 import com.cpms.pojos.Project;
 
 public interface IProjectService {
 	
 	Project registerProject(Project project);
 	List<Project> getAllProjectList(Course course);
-	List<Project> getProjectsWithNoGuide();
 	List<Project> getProjectsWithNoGuide(/*Course courseName*/);
 }
